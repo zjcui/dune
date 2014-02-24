6b28f669b352450db239a1e7918eee9cb24eb7d9
Joao Fortuna <fortuna.araujo@gmail.com>
2014-02-24 15:33:38 +0000

    Monitors/UAV/Takeoff: First version of Monitor with support for Rolling and Catapult takeoff. Testing required.

--- src/Monitors/UAV/Takeoff/Task.cpp
+++ src/Monitors/UAV/Takeoff/Task.cpp
@@ -54,52 +54,112 @@ namespace Monitors
         TKOFF_DROP = 3
       };
 
+      //! Takeoff stage specifier
+      enum TakeoffStage
+      {
+        //! Waiting for takeoff request
+        GROUND = 0,
+        //! Takeoff requested, waiting for launch
+        PRELAUNCH = 1,
+        //! Launch conditions were met,
+        //! if kept then we are flying
+        LAUNCH = 2,
+        //! Takeoff successful
+        FLYING = 3
+      };
+
+
       //! %Task arguments.
       struct Arguments
       {
         //! Air Speed threshold.
         float airspeed_threshold;
+        //! Acceleration threshold.
+        float acc_threshold;
+        //! Takeoff Altitude threshold.
+        float tkoff_alt_threshold;
+        //! Flying Altitude threshold.
+        float flying_alt_threshold;
         //! Takeoff Type.
         std::string type;
+        //! Takeoff timer
+        float timer;
       };
 
-      //! %Medium task.
-      struct Task: public DUNE::Tasks::Periodic
+      struct Task: public DUNE::Tasks::Task
       {
-        //! Vehicle Medium.
-        IMC::VehicleMedium m_vm;
-        //! Initialization timer.
-        Time::Counter<float> m_init;
-        //! Vehicle airspeed.
-        float m_airspeed;
+        //! Launch timer.
+        Time::Counter<float> m_timer;
         //! Task arguments.
         Arguments m_args;
-        //! Aircraft Type
+        //! Takeoff Type
         Takeoff m_tkoff_method;
+        //! Takeoff state machine stage
+        TakeoffStage m_tkoff_stage;
+        //! Vehicle airspeed.
+        float m_airspeed;
+        //! Takeoff requested
+        bool m_tkoff_req;
+        //! Is on ground
+        bool m_ground;
+        //! Ground height.
+        float m_gnd_hei;
+        //! Vehicle altitude above ground.
+        float m_alt;
+        //! Vehicle longitudinal acceleration.
+        double m_acc;
 
         Task(const std::string& name, Tasks::Context& ctx):
-          Tasks::Periodic(name, ctx),
+          Tasks::Task(name, ctx),
+          m_tkoff_method(TKOFF_CATAPULT),
+          m_tkoff_stage(GROUND),
           m_airspeed(0),
-          m_tkoff_method(TKOFF_CATAPULT)
+          m_tkoff_req(false),
+          m_ground(true),
+          m_gnd_hei(0),
+          m_alt(0),
+          m_acc(0)
         {
-          param("Air Speed Threshold", m_args.airspeed_threshold)
+          param("Takeoff Altitude Threshold", m_args.tkoff_alt_threshold)
           .units(Units::Meter)
+          .defaultValue("10.0")
+          .description("Minimum altitude necessary to consider a vehicle has been launched");
+
+          param("Flying Altitude Threshold", m_args.flying_alt_threshold)
+          .units(Units::Meter)
+          .defaultValue("50.0")
+          .description("Minimum altitude necessary to consider a vehicle flying");
+
+          param("Air Speed Threshold", m_args.airspeed_threshold)
+          .units(Units::MeterPerSecond)
           .defaultValue("12.0")
           .description("Minimum air speed necessary to consider a vehicle in air");
 
+          param("Acceleration Threshold", m_args.acc_threshold)
+          .units(Units::MeterPerSquareSecond)
+          .defaultValue("1.0")
+          .description("Minimum acceleration necessary to consider a good catapult/hand launch");
+
+          param("Launch Timer", m_args.timer)
+          .units(Units::Second)
+          .defaultValue("12.0")
+          .description("Minimum time in launch stage necessary to consider a vehicle flying");
+
           param("Takeoff Method", m_args.type)
-          .defaultValue("LAUNCH")
-          .values("ROLLING, LAUNCH, VERTICAL, DROP")
+          .defaultValue("CATAPULT")
+          .values("ROLLING, CATAPULT, VERTICAL, DROP")
           .description("Method of takeoff");
 
           // Register consumers.
           bind<IMC::EstimatedState>(this);
           bind<IMC::IndicatedSpeed>(this);
           bind<IMC::VehicleMedium>(this);
+          bind<IMC::DesiredPath>(this);
+          bind<IMC::Acceleration>(this);
         }
 
         void
-        onUpdateParameters(void)
+        onResourceInitialization(void)
         {
           if (m_args.type == "ROLLING")
             m_tkoff_method = TKOFF_ROLLING;
@@ -111,16 +171,42 @@ namespace Monitors
             m_tkoff_method = TKOFF_DROP;
           else
             err(DTR("Unrecognized Takeoff method"));
+
+          m_timer.setTop(m_args.timer);
         }
 
         void
         consume(const IMC::EstimatedState* msg)
         {
+          if (m_tkoff_stage == GROUND)
+          {
+            m_gnd_hei = msg->height - msg->z;
+            m_alt = 0;
+          }
+          else
+          {
+            float hei = msg->height - msg->z;
+            m_alt = hei - m_gnd_hei;
+          }
         }
 
         void
         consume(const IMC::VehicleMedium* msg)
         {
+          m_ground = (msg->medium == VehicleMedium::VM_GROUND);
+        }
+
+        void
+        consume(const IMC::Acceleration* msg)
+        {
+          m_acc = msg->x;
+        }
+
+        void
+        consume(const IMC::DesiredPath* msg)
+        {
+//          TODO: Add Takeoff and Land flags to DesiredPath
+//          m_tkoff_req = (msg->flags && DesiredPath::FL_TAKEOFF);
         }
 
         void
@@ -130,37 +216,74 @@ namespace Monitors
         }
 
         void
-        task(void)
+        onMain(void)
         {
-          // Wait to stabilize at beginning.
-          if (!m_init.overflow())
-            return;
+          setEntityState(IMC::EntityState::ESTA_BOOT, Status::CODE_INIT);
 
-          // Initialization.
-          if (getEntityState() == IMC::EntityState::ESTA_BOOT)
+          while (!stopping())
           {
-            if (m_airspeed < m_args.airspeed_threshold)
-              m_vm.medium = IMC::VehicleMedium::VM_GROUND;
-            else
-              m_vm.medium = IMC::VehicleMedium::VM_AIR;
+            //! State machine
+            if (m_tkoff_method == TKOFF_ROLLING)
+            {
+              switch (m_tkoff_stage)
+              {
+                case GROUND:
+                  if (m_tkoff_req)
+                    m_tkoff_stage = PRELAUNCH;
+                  m_tkoff_req = false;
+                  break;
+                case PRELAUNCH:
+                  if (m_airspeed > 1.1 * m_args.airspeed_threshold)
+                  {
+                    m_tkoff_stage = LAUNCH;
+                    m_timer.reset();
+                  }
+                  break;
+                case LAUNCH:
+                  if (m_airspeed < 0.95 * m_args.airspeed_threshold)
+                    m_tkoff_stage = PRELAUNCH;
+                  else if ((m_timer.overflow() && m_alt > m_args.tkoff_alt_threshold) ||
+                           m_alt > m_args.flying_alt_threshold)
+                    m_tkoff_stage = FLYING;
 
-            setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
-            dispatch(m_vm);
+                  break;
+                case FLYING:
+                  setEntityState(IMC::EntityState::ESTA_NORMAL, "Flying");
+                  break;
+              }
+            }
+            else if (m_tkoff_method == TKOFF_CATAPULT)
+            {
+              switch (m_tkoff_stage)
+              {
+                case GROUND:
+                  if (m_tkoff_req)
+                    m_tkoff_stage = PRELAUNCH;
+                  m_tkoff_req = false;
+                  break;
+                case PRELAUNCH:
+                  if (m_acc > m_args.acc_threshold ||
+                      m_airspeed > 1.1 * m_args.airspeed_threshold)
+                  {
+                    m_tkoff_stage = LAUNCH;
+                    m_timer.reset();
+                  }
+                  break;
+                case LAUNCH:
+                  if (m_airspeed < 0.95 * m_args.airspeed_threshold)
+                    m_tkoff_stage = PRELAUNCH;
+                  else if ((m_timer.overflow() && m_alt > m_args.tkoff_alt_threshold) ||
+                           m_alt > m_args.flying_alt_threshold)
+                    m_tkoff_stage = FLYING;
 
-            return;
-          }
+                  break;
+                case FLYING:
+                  setEntityState(IMC::EntityState::ESTA_NORMAL, "Flying");
+                  break;
+              }
+            }
 
-          // Task state machine.
-          switch (m_vm.medium)
-          {
-            case IMC::VehicleMedium::VM_AIR:
-              if (m_airspeed < m_args.airspeed_threshold)
-                m_vm.medium = IMC::VehicleMedium::VM_GROUND;
-              break;
-            case IMC::VehicleMedium::VM_GROUND:
-              if (m_airspeed > m_args.airspeed_threshold && m_args.type == "UAV")
-                m_vm.medium = IMC::VehicleMedium::VM_AIR;
-              break;
+            waitForMessages(1.0);
           }
         }
       };
