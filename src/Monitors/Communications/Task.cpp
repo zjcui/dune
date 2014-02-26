889b3e1e13d5c6135010320f3d77a0b44b5042de
Joao Fortuna <fortuna.araujo@gmail.com>
2014-02-26 09:22:58 +0000

    Monitors/Communications: Moved from Monitors/UAV/Communications.

--- src/Monitors/Communications/Task.cpp
+++ src/Monitors/Communications/Task.cpp
@@ -27,93 +27,88 @@
 
 // ISO C++ 98 headers.
 #include <cstring>
-#include <queue>
-#include <cmath>
 
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
 
 namespace Monitors
 {
-  namespace UAV
+  namespace Communications
   {
-    namespace Communications
+    using DUNE_NAMESPACES;
+
+    struct Arguments
     {
-      using DUNE_NAMESPACES;
+      //! Heartbeat timeout.
+      float hrtbeat_tout;
+      //! Timeout if executing plan
+      float mission_tout;
+    };
 
-      struct Arguments
+    struct Task: public DUNE::Tasks::Periodic
+    {
+      //! Time of last heartbeat.
+      double m_hrtbeat_last;
+      //! True if executing plan
+      bool m_in_mission;
+      //! Task arguments.
+      Arguments m_args;
+
+      Task(const std::string& name, Tasks::Context& ctx):
+        Tasks::Periodic(name, ctx),
+        m_hrtbeat_last(0),
+        m_in_mission(false)
       {
-        //! Heartbeat timeout.
-        float hrtbeat_tout;
-        //! Timeout if executing plan
-        float mission_tout;
-      };
+        param("Heartbeat Timeout", m_args.hrtbeat_tout)
+        .units(Units::Second)
+        .defaultValue("5.0")
+        .description("Heartbeat Timeout");
 
-      struct Task: public DUNE::Tasks::Periodic
+        param("In Mission Timeout", m_args.mission_tout)
+        .units(Units::Second)
+        .defaultValue("10.0")
+        .description("Timeout if executing plan");
+
+        bind<IMC::Heartbeat>(this);
+        bind<IMC::PlanControlState>(this);
+      }
+
+      void
+      onResourceInitialization(void)
       {
-        //! Time of last heartbeat.
-        double m_hrtbeat_last;
-        //! True if executing plan
-        bool m_in_mission;
-        //! Task arguments.
-        Arguments m_args;
+        // Initialize entity state.
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
 
-        Task(const std::string& name, Tasks::Context& ctx):
-          Tasks::Periodic(name, ctx),
-          m_hrtbeat_last(0),
-          m_in_mission(false)
-        {
-          param("Heartbeat Timeout", m_args.hrtbeat_tout)
-          .units(Units::Second)
-          .defaultValue("5.0")
-          .description("Heartbeat Timeout");
+        m_hrtbeat_last = Clock::get();
+      }
 
-          param("In Mission Timeout", m_args.mission_tout)
-          .units(Units::Second)
-          .defaultValue("10.0")
-          .description("Timeout if executing plan");
+      void
+      consume(const IMC::Heartbeat* msg)
+      {
+        if (msg->getSource() == getSystemId())
+          return;
 
-          bind<IMC::Heartbeat>(this);
-          bind<IMC::PlanControlState>(this);
-        }
+        m_hrtbeat_last = Clock::get();
+      }
 
-        void
-        onResourceInitialization(void)
-        {
-          // Initialize entity state.
+      void
+      consume(const IMC::PlanControlState* msg)
+      {
+        m_in_mission = (msg->state & IMC::PlanControlState::PCS_EXECUTING) != 0;
+      }
+
+      void
+      task(void)
+      {
+        double now = Clock::get();
+
+        if ((now > (m_hrtbeat_last + m_args.mission_tout) &&  m_in_mission) ||
+            (now > (m_hrtbeat_last + m_args.hrtbeat_tout) && !m_in_mission))
+          setEntityState(IMC::EntityState::ESTA_FAULT, Status::CODE_COM_ERROR);
+        else
           setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
-
-          m_hrtbeat_last = Clock::get();
-        }
-
-        void
-        consume(const IMC::Heartbeat* msg)
-        {
-          if (msg->getSource() == getSystemId())
-            return;
-
-          m_hrtbeat_last = Clock::get();
-        }
-
-        void
-        consume(const IMC::PlanControlState* msg)
-        {
-          m_in_mission = (msg->state & IMC::PlanControlState::PCS_EXECUTING) != 0;
-        }
-
-        void
-        task(void)
-        {
-          double now = Clock::get();
-
-          if ((now > (m_hrtbeat_last + m_args.mission_tout) &&  m_in_mission) ||
-              (now > (m_hrtbeat_last + m_args.hrtbeat_tout) && !m_in_mission))
-            setEntityState(IMC::EntityState::ESTA_FAULT, Status::CODE_COM_ERROR);
-          else
-            setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
-        }
-      };
-    }
+      }
+    };
   }
 }
 
