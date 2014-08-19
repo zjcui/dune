5d05a185da5b23a2d081906c8ab0db3d6933335f
Ricardo Martins <rasm@lsts.pt>
2014-08-19 01:46:10 +0100

    Prograns: test_Network_TCPSocket: Added test for DUNE::Network::TCPSocket class.

---
+++ programs/tests/test_Network_TCPSocket.cpp
@@ -0,0 +1,97 @@
+//***************************************************************************
+// Copyright 2007-2014 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://www.lsts.pt/dune/licence.                                        *
+//***************************************************************************
+// Author: Ricardo Martins                                                  *
+//***************************************************************************
+
+// ISO C++ 98 headers.
+#include <iostream>
+
+// DUNE headers.
+#include <DUNE/Network.hpp>
+#include <DUNE/Time.hpp>
+
+// Local headers.
+#include "Test.hpp"
+
+using namespace DUNE::Network;
+using namespace DUNE::Time;
+
+int
+main(void)
+{
+  Test test("Network::TCPSocket");
+
+  // Try connecting to a server that drops packets to a given port. This
+  // will generate a connection timeout.
+  {
+    TCPSocket s;
+
+    try
+    {
+      s.connect("8.8.8.8", 32999, 2.0);
+      test.failed("TCPSocket::connect() - Timeout");
+    }
+    catch (ConnectionTimeout& e)
+    {
+      std::cerr << e.what() << std::endl;
+      test.passed("TCPSocket::connect() - Timeout");
+    }
+  }
+
+  // Try connecting to a port that doesn't have any service running. This
+  // will generate a connection error.
+  {
+    TCPSocket s;
+
+    try
+    {
+      s.connect("127.0.0.1", 32999, 10.0);
+      test.failed("TCPSocket::connect() - Error");
+    }
+    catch (ConnectionError& e)
+    {
+      std::cerr << e.what() << std::endl;
+      test.passed("TCPSocket::connect() - Error");
+    }
+  }
+
+  // Try connecting to a port with a service running. This will establish
+  // a connection.
+  {
+    TCPSocket s;
+
+    try
+    {
+      s.connect("www.google.com", 80, 10.0);
+      test.passed("TCPSocket::connect() - Success");
+    }
+    catch (ConnectionError& e)
+    {
+      test.failed("TCPSocket::connect() - Success");
+    }
+  }
+
+  return 0;
+}
