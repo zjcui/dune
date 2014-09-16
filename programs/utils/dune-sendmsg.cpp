f75ac48119ad746db3bb89528b786c3ad31de8ca
Renato Caldas <rmsc@fe.up.pt>
2014-09-16 16:36:13 +0100

    Programs: dune-sendmsg: Added message EntityActivationLock.

--- programs/utils/dune-sendmsg.cpp
+++ programs/utils/dune-sendmsg.cpp
@@ -53,7 +53,7 @@ main(int argc, char** argv)
       fprintf(stdout, "  DataSanity, DesiredControl, DesiredHeading, DesiredHeadingRate, DesiredPitch\n");
       fprintf(stdout, "  DesiredSpeed, DesiredRoll, DesiredZ, DevCalibrationControl, DevDataText\n");
       fprintf(stdout, "  EmergencyControl, EntityList, EntityState, EntityActivationState, EstimatedState\n");
-      fprintf(stdout, "  FuelLevel\n");
+      fprintf(stdout, "  EntityActivationLock, FuelLevel\n");
       fprintf(stdout, "  GpsFix, Heartbeat, IridiumMsgTx, LblConfig, LblRange\n");
       fprintf(stdout, "  LeakSimulation, LogBookControl, LogBookEntry, LoggingControl\n");
       fprintf(stdout, "  MagneticField, MonitorEntityState, OperationalLimits\n");
@@ -298,6 +298,17 @@ main(int argc, char** argv)
     tmsg->lat = 0.0;
     tmsg->lon = 0.0;
     tmsg->depth = 0.0;
+  }
+
+  if (strcmp(argv[3], "EntityActivationLock") == 0)
+  {
+    IMC::EntityActivationLock* tmsg = new IMC::EntityActivationLock;
+    msg = tmsg;
+    tmsg->setDestinationEntity(atoi(argv[4]));
+    if (argc > 5)
+      tmsg->op = atoi(argv[5]);
+    if (argc > 6)
+      tmsg->timeout = atoi(argv[6]);
   }
 
   if (strcmp(argv[3], "FuelLevel") == 0)
